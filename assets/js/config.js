412c4425c6b3390780c02a7cc507816efda0d6fa
root <root@geo.agrisoft.co.id>
2017-11-26 13:01:09 +0700

    config

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var _baseURL = ""; 
+// URL CSW 
+var _cswURL = 'http://192.168.100.55/csw'; 
+// URL WMS 
+var _wmsURL = 'http://192.168.100.55:8080/geoserver/wms?'; 
+// URL API 
+var _api = 'http://192.168.100.55:8000/api/'; 
+// URL PROXY if null not use Proxy
+var _proxy = 'http://192.168.100.55:8000/api/proxy?url=';
